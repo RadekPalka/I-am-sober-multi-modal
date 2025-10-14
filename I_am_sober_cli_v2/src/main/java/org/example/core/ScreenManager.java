acd57e7f91a666111545b3a199f14e37397bf1b4
radek <radekpalka@gmail.com>
2025-10-14 19:50:53 +0200

    feat: decide initial screen based on saved token

--- I_am_sober_cli_v2/src/main/java/org/example/core/ScreenManager.java
+++ I_am_sober_cli_v2/src/main/java/org/example/core/ScreenManager.java
@@ -1,26 +1,44 @@
 package org.example.core;
 
 import com.example.auth.Session;
+import com.example.client.ApiClient;
+import com.example.dto.UserDto;
 import com.example.service.SessionTokenStore;
 import org.example.screen.DashboardScreen;
 import org.example.screen.HomeScreen;
 import java.util.Optional;
 
 public class ScreenManager {
-    private HomeScreen homescreen;
+    private HomeScreen homeScreen;
     private DashboardScreen dashboardScreen;
     private Session session;
+    private ApiClient apiClient;
 
-    public ScreenManager(HomeScreen homeScreen, DashboardScreen dashboardScreen, Session session){
-        this.homescreen = homeScreen;
+    public ScreenManager(HomeScreen homeScreen, DashboardScreen dashboardScreen, Session session, ApiClient apiClient) {
+        this.homeScreen = homeScreen;
         this.dashboardScreen = dashboardScreen;
         this.session = session;
+        this.apiClient = apiClient;
     }
 
-    public String loadTokenIfExists(){
-        Optional<String> token = SessionTokenStore.loadToken();
-        token.ifPresent(session::setToken);
+    public void decideInitialScreen() {
+        Optional<String> tokenOpt = SessionTokenStore.loadToken();
+
+        if (tokenOpt.isPresent()) {
+            tokenOpt.flatMap(apiClient::fetchUser)
+                    .ifPresentOrElse(
+                            user -> {
+                                session.setLogin(user.getUsername());
+                                session.setToken(tokenOpt.get());
+                                dashboardScreen.init();
+                            },
+                            () -> homeScreen.init()
+                    );
+        }
+        else{
+            homeScreen.init();
+        }
+
 
     }
-
 }
